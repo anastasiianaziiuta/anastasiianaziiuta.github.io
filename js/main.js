51d2506bdf936dd88aa54a58482ea04c07652798
Anastasiia <anastasiia.naziiuta@gmail.com>
2021-01-26 20:03:54 +0200

    scripts

--- js/main.js
+++ js/main.js
@@ -1,65 +1,63 @@
+window.addEventListener('DOMContentLoaded', function (){
 
-let block = document.querySelector('#slider'),
-    slideWith = document.querySelector('#slide'),
-    showResult = document.querySelector('.result'),
-    boxleft, // положение блока по левой стороне
-    startx, // стартовая точка соприкосновения по x
-    dist = 0, // расстояние перемещения точки
-    touchobj = null; // содержимое объекта перемещения
+  let block = document.querySelector('#slider'),
+      slideWith = document.querySelector('#slide'),
+      showResult = document.querySelector('.result'),
+      boxleft, // положение блока по левой стороне
+      startx, // стартовая точка соприкосновения по x
+      dist = 0, // расстояние перемещения точки
+      touchobj = null; // содержимое объекта перемещения
 
-    block.addEventListener('touchstart', function(e){
-          e.preventDefault(); 
-          touchobj = e.changedTouches[0];
-          boxleft = parseInt(block.style.left);
-          startx = parseInt(touchobj.clientX); 
-    }, false);
-
-    block.addEventListener('touchmove', function(e){
-          e.preventDefault();
-          let dist = parseInt(touchobj.clientX) - startx;
+      block.addEventListener('touchstart', function(e){
+        console.log('start');
+            e.preventDefault(); 
             touchobj = e.changedTouches[0];
-            block.style.left = ( (boxleft + dist > slideWith.offsetWidth)? slideWith.offsetWidth : (boxleft + dist < 0)? 0 : boxleft + dist ) + 'px';
-            block.innerHTML = Math.round(block.offsetLeft * 50000 / slideWith.offsetWidth);
-            function showCost (){
-                showResult.innerHTML = Math.round( (block.offsetLeft * 50000 / slideWith.offsetWidth) * 20 );
-            }    
-            showCost ()    
-    }, false);
+            boxleft = block.style.left;
+            startx = touchobj.clientX;
 
+            block.addEventListener('touchmove', function(e){
+              console.log('move');
+              e.preventDefault();
+                dist = touchobj.clientX - startx;
+                touchobj = e.changedTouches[0];
+                block.style.left = ( (boxleft + dist > slideWith.offsetWidth)? slideWith.offsetWidth : (boxleft + dist < 0)? 0 : boxleft + dist ) + 'px';
+                block.innerHTML = Math.round(block.offsetLeft * 50000 / slideWith.offsetWidth);
+                function showCost (){
+                    showResult.innerHTML = Math.round( (block.offsetLeft * 50000 / slideWith.offsetWidth) * 20 );
+                }    
+                showCost ();   
+          }, false); 
 
-    block.onmousedown = function(e) {
-      e.preventDefault(); 
-      let shiftX = e.clientX - block.getBoundingClientRect().left;
-      document.addEventListener('mousemove', onMouseMove);
-      document.addEventListener('mouseup', onMouseUp);
+      }, false);
  
-      function onMouseMove(e) {
-        let newLeft = e.clientX - shiftX - slide.getBoundingClientRect().left;
-        if (newLeft < 0) {
-          newLeft = 0;
+      block.addEventListener('mousedown', onMouseDown);
+      function onMouseDown(e){
+        e.preventDefault(); 
+        
+        startx = e.clientX - block.getBoundingClientRect().left;
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mouseup', onMouseUp);
+  
+        function onMouseMove(e) {
+          boxleft = e.clientX - startx - slide.getBoundingClientRect().left;
+          
+          block.style.left = ( (boxleft + dist > slideWith.offsetWidth)? slideWith.offsetWidth : (boxleft + dist < 0)? 0 : boxleft + dist ) + 'px';
+          block.innerHTML = Math.round(block.offsetLeft * 50000 / slideWith.offsetWidth);
+          function showCost (){
+              showResult.innerHTML = Math.round( (block.offsetLeft * 50000 / slideWith.offsetWidth) * 20 );
+          }    
+          showCost ();
+
         }
-        let rightEdge = slide.offsetWidth;
-        if (newLeft > rightEdge) {
-          newLeft = rightEdge;
+        function onMouseUp() {
+          document.removeEventListener('mouseup', onMouseUp);
+          document.removeEventListener('mousemove', onMouseMove);
+
         }
-        block.style.left = newLeft + 'px';
-
-            block.innerHTML = Math.round(block.offsetLeft * 50000 / slideWith.offsetWidth);
-        function showCost (){
-            showResult.innerHTML = Math.round( (block.offsetLeft * 50000 / slideWith.offsetWidth) * 20 );
-        }    
-        showCost ()
-
       }
-      function onMouseUp() {
-        document.removeEventListener('mouseup', onMouseUp);
-        document.removeEventListener('mousemove', onMouseMove);
+  });    
+ 
 
-      }
-    };
-    block.ondragstart = function() {
-      return false;
-    };
 
 
 
