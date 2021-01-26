e5f5e1045181e948de978ceca13bcb2cd884bd01
Anastasiia <anastasiia.naziiuta@gmail.com>
2021-01-26 18:23:51 +0200

    add scripts

--- js/main.js
+++ js/main.js
@@ -1,16 +1,38 @@
-// Slider
-let block = slide.querySelector('#slider');
+
+let block = document.querySelector('#slider'),
+    slideWith = document.querySelector('#slide'),
+    showResult = document.querySelector('.result'),
+    boxleft, // положение блока по левой стороне
+    startx, // стартовая точка соприкосновения по x
+    dist = 0, // расстояние перемещения точки
+    touchobj = null; // содержимое объекта перемещения
+
+    block.addEventListener('touchstart', function(e){
+          e.preventDefault(); 
+          touchobj = e.changedTouches[0];
+          boxleft = parseInt(block.style.left);
+          startx = parseInt(touchobj.clientX); 
+    }, false);
+
+    block.addEventListener('touchmove', function(e){
+          e.preventDefault();
+          let dist = parseInt(touchobj.clientX) - startx;
+            touchobj = e.changedTouches[0];
+            block.style.left = ( (boxleft + dist > slideWith.offsetWidth)? slideWith.offsetWidth : (boxleft + dist < 0)? 0 : boxleft + dist ) + 'px';
+            block.innerHTML = Math.round(block.offsetLeft * 50000 / slideWith.offsetWidth);
+            function showCost (){
+                showResult.innerHTML = Math.round( (block.offsetLeft * 50000 / slideWith.offsetWidth) * 20 );
+            }    
+            showCost ()    
+    }, false);
+
 
     block.onmousedown = function(e) {
       e.preventDefault(); 
       let shiftX = e.clientX - block.getBoundingClientRect().left;
       document.addEventListener('mousemove', onMouseMove);
       document.addEventListener('mouseup', onMouseUp);
-      document.addEventListener("touchstart", handleStart, false);
-      document.addEventListener("touchend", handleEnd, false);
-      document.addEventListener("touchcancel", handleCancel, false);
-      document.addEventListener("touchmove", handleMove, false);
-
+ 
       function onMouseMove(e) {
         let newLeft = e.clientX - shiftX - slide.getBoundingClientRect().left;
         if (newLeft < 0) {
@@ -22,8 +44,6 @@ let block = slide.querySelector('#slider');
         }
         block.style.left = newLeft + 'px';
 
-        let slideWith = document.querySelector('#slide'),
-            showResult = document.querySelector('.result');
             block.innerHTML = Math.round(block.offsetLeft * 50000 / slideWith.offsetWidth);
         function showCost (){
             showResult.innerHTML = Math.round( (block.offsetLeft * 50000 / slideWith.offsetWidth) * 20 );
@@ -34,17 +54,13 @@ let block = slide.querySelector('#slider');
       function onMouseUp() {
         document.removeEventListener('mouseup', onMouseUp);
         document.removeEventListener('mousemove', onMouseMove);
-        document.removeEventListener("touchstart", handleStart, false);
-        document.removeEventListener("touchend", handleEnd, false);
-        document.removeEventListener("touchcancel", handleCancel, false);
-        document.removeEventListener("touchmove", handleMove, false);
 
       }
     };
     block.ondragstart = function() {
       return false;
     };
-    // Calc
+
 
 
   
